8ea5b98817adf0e87401dcf1247299034080cfb3
eswaribala <Parameswaribala@gmail.com>
2017-06-30 15:44:30 +0530

    Add files via upload

---
+++ styleexample.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by BALASUBRAMANIAM on 04-01-2017.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-style-example',
+    template: `
+    <p  style="padding: 1rem"
+        [ngStyle]="{ 
+          color: 'red',
+          'font-weight': 'bold',
+          borderBottom: borderStyle
+        }">
+        <ng-content></ng-content>
+    </p>
+  `
+})
+export class StyleExampleComponent {
+    //borderStyle = '1px solid black';
+}
